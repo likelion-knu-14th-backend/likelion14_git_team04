fd2fb4f7efb4da5a3b018c4baab3860dabdf68a2
doodlios <soakaeofhg@naver.com>
2026-03-26 19:29:37 +0900

    hello2

--- src/main/java/com/likelion14/session/data/Example.java
+++ src/main/java/com/likelion14/session/data/Example.java
@@ -3,6 +3,7 @@ package com.likelion14.session.data;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
     }
 }
